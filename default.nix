4fae7717c93ebc2c8245496f3670e7e79d4865cf
Tor Hedin Brønner <torhedinbronner@gmail.com>
2017-12-10 17:32:24 +0100

    Create symlinks on installation for proper lazy loading
    bash-completion expects one file per command when lazy loading completion
    scripts.

--- default.nix
+++ default.nix
@@ -2,7 +2,15 @@ with import <nixpkgs> {}; stdenv.mkDerivation rec {
   name = "nix-bash-completions";
   src = ./.;
   installPhase = ''
-    mkdir -p $out/share/bash-completion/completions
-    cp _nix $out/share/bash-completion/completions
+    commands=$(
+      function complete() { shift 2; echo "$@"; }
+      shopt -s extglob
+      source _nix
+    )
+    install -Dm444 -t $out/share/bash-completion/completions _nix
+    cd $out/share/bash-completion/completions
+    for c in $commands; do
+      ln -s _nix $c
+    done
   '';
 }
